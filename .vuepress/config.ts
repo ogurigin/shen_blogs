64a001d7a6802c3716d2455b920749598fa33089
oguri_Xx <315980986@qq.com>
2022-06-10 15:51:33 +0800

    init

---
+++ .vuepress/config.ts
@@ -0,0 +1,48 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+import recoTheme from 'vuepress-theme-reco'
+
+export default defineUserConfig({
+  title: 'shen',
+  description: '走',
+  theme: recoTheme({
+    style: '@vuepress-reco/style-default',
+    logo: '/logo.png',
+    author: 'shen',
+    docsRepo: '一些记录',
+    docsBranch: 'main',
+    docsDir: 'example',
+    lastUpdatedText: '',
+    // series 为原 sidebar
+    series: {
+      '/docs/code/': [
+        {
+          text: '前端',
+          children: ['home', 'theme']
+        },
+      ]
+    },
+    navbar:
+    [
+      { text: 'Home', link: '/' },
+      { text: 'vue', link: '/categories/reco/1/' },
+      { text: '关于',
+        children: [
+          { text: '关于我', link: '/about/me' },
+          { text: '导航', link: '/blogs/other/guide' }
+        ]
+      },
+    ],
+    // valineConfig 配置与 1.x 一致
+    // valineConfig: {
+    //   appId: 'xxx',
+    //   appKey: 'xxx',
+    //   placeholder: '填写邮箱可以收到回复提醒哦！',
+    //   verify: true, // 验证码服务
+    //   // notify: true,
+    //   recordIP: true,
+    //   // hideComments: true // 隐藏评论
+    // },
+  }),
+  // debug: true,
+})
---
+++ README.md
@@ -0,0 +1,63 @@
+---
+home: true
+modules:
+  - banner
+  - Blog
+  # - MdContent
+  - Footer
+banner:
+  heroImage: /logo.png
+  heroImageStyle:
+    maxWidth: '200px'
+    width: '100%'
+    display: block
+    margin: '0 auto 2rem'
+    borderRadius: '1rem'
+  bgImage: '/bg.svg'
+  heroText: Shen
+  tagline: 一些东西
+blog:
+  socialLinks:
+    - { icon: 'BrandGithub', link: 'https://github.com/ogurigin' }  
+    - { icon: 'Steam', link: 'https://github.com/ogurigin' }  
+  author: Shen
+footer: # 底部模块的配置
+  record: go
+  recordLink: shen
+  startYear: 2022
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+features:
+- title: 过去
+  details: 开发一款看着开心、写着顺手的 vuepress 博客主题。
+- title: 当下
+  details: 帮助更多的朋友节省时间去用心书写内容，而不是仅仅配置一个博客去孤芳自赏。
+- title: 未来
+  details: 吸引更多的朋友参与到开发中来，继续强大功能。
+---
+
+<!-- ## 快速开始
+
+**npx**
+
+```bash
+# 初始化，并选择 2.x
+npx @vuepress-reco/theme-cli init
+```
+
+**npm**
+
+```bash
+# 初始化，并选择 2.x
+npm install @vuepress-reco/theme-cli@1.0.7 -g
+theme-cli init
+```
+
+**yarn**
+
+```bash
+# 初始化，并选择 2.x
+yarn global add @vuepress-reco/theme-cli@1.0.7
+theme-cli init
+``` -->
